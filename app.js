b6005de64cc657ab12bc53da26a18a76d95eb0a9
minjun <rhalswns098@gmail.com>
2023-12-19 17:27:44 +0900

    update:DAY16 실습

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
