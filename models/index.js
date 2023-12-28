c0df7d3a341dd9bec96240bf2dca0852a8167ca4
minjun <rhalswns098@gmail.com>
2023-12-28 15:18:55 +0900

    new:sequelize 프레임워크 환경 만들기, model/index 설정

---
+++ models/index.js
@@ -0,0 +1,24 @@
+
+const path = require('path');
+const Sequelize = require('sequelize');
+
+
+const env = process.env.NODE_ENV || 'development';
+const config = require(path.join(__dirname + '/../config/config.json'))[env];
+
+
+const db = {};
+
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+
+// 모델 연결
+// db.Admin = require('./models/admin')(sequelize,Sequelize);
+
+
+module.exports = db;
