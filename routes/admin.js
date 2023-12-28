88d4b4beb045b7c5fbd67f4363c44c796fc33766
minjun <rhalswns098@gmail.com>
2023-12-28 16:37:32 +0900

    삭제 링크 추가

--- routes/admin.js
+++ routes/admin.js
@@ -37,12 +37,12 @@ router.post('/list', async function (req, res, next) {
 
 
 
-router.get('/create', async(req,res)=>{
-  res.render('admin/create');
-});
 
 
 // 담당 : 고민준
+router.get('/create', async(req,res)=>{
+  res.render('admin/create');
+});
 
 var resultMsg = {
   code: 200,
@@ -109,11 +109,11 @@ router.get('/modify/:aid', async(req,res)=>{
   var admin_member_id = req.params.aid;
 
   // admin id 데이터로 DB정보 찾기
-  var adminList = await db.Admin.findOne({where:{admin_member_id:admin_member_id}});
+  var admin_member = await db.Admin.findOne({where:{admin_member_id:admin_member_id}});
 
   
 
-  res.render('admin/modify', {adminList});
+  res.render('admin/modify', {admin_member});
 });
 
 
