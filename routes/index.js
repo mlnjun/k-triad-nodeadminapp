64b842b32a6b6dd0347633b7ba53459a87a15cdd
ursrBaek <ursrbaek@gmail.com>
2023-12-29 11:46:08 +0900

    feat: 로그인 실패 후 렌더 시 입력값 유지 적용

--- routes/index.js
+++ routes/index.js
@@ -1,62 +1,58 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var db = require('../models/index');
-
+var db = require("../models/index");
 
 /*
 -메인 페이지 요청 라우팅 메소드
 -호출 주소 : http://localhost:3001
 */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get("/", function (req, res, next) {
+  res.render("index");
 });
-
 
 /*
 -로그인 페이지 요청 라우팅 메소드
 -호출 주소 : http://localhost:3001/login
 */
-router.get('/login', async(req,res)=>{
-  res.render('login', { layout: false, resultMsg:""});
+router.get("/login", async (req, res) => {
+  res.render("login", {
+    layout: false,
+    resultMsg: "",
+    admin_id: "",
+    admin_password: "",
+  });
 });
-
 
 /*
 -로그인 페이지 요청과 응답 라우팅 메소드
 -호출 주소 : http://localhost:3001/login
 */
-router.post('/login',async(req,res)=>{
+router.post("/login", async (req, res) => {
   var admin_id = req.body.admin_id;
   var admin_password = req.body.admin_password;
 
-
   // DB에서 해당 id 정보 찾기
-  var admin_member = await db.Admin.findOne({where:{admin_id:admin_id}});
-
+  var admin_member = await db.Admin.findOne({ where: { admin_id: admin_id } });
 
   let resultMsg = "";
 
-  if(admin_member == null){
+  if (admin_member == null) {
     // 아이디 틀림
     resultMsg = "해당 아이디가 존재하지 않습니다.";
-  }else{
-    if(admin_member.admin_password == admin_password){
+  } else {
+    if (admin_member.admin_password == admin_password) {
       // 로그인 성공
-      res.redirect('/');
-    }else{
+      res.redirect("/");
+    } else {
       // 비밀번호 틀림
       resultMsg = "해당 아이디의 비밀번호가 아닙니다.";
     }
   }
 
-
-  if(resultMsg !== ""){
-    res.render('login',{layout:false, resultMsg});
+  if (resultMsg !== "") {
+    res.render("login", { layout: false, resultMsg, admin_id, admin_password });
   }
 });
-
-
-
 
 module.exports = router;
